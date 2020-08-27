42e44b49348e03b4edefab5628c29ef839377876
Caitlin Crawford <caitlin.crawford16@gmail.com>
2020-08-27 18:47:20 -0400

    Add revisions and comments to js code

--- index.js
+++ index.js
@@ -28,16 +28,18 @@ $(document).ready( () => {
 })
 
 function saveToWatchlist(imdbID) {
-    var movie = movieData.find((currentMovie) => {
-        return currentMovie.imdbID == imdbID;
+    var movie = movieData.find((currentMovie) => { //find method only returns a single element; use filter method if want to return multiple values
+        return currentMovie.imdbID === imdbID; //compares currentMovie.imdb and returns value only if the same as currentMovie.imdbID within the onclick event
     });
 
-    var watchlistJSON = localStorage.getItem("watchlist");
-    var watchlist = JSON.parse(watchlistJSON);
-    if (watchlist === null) {
+    var watchlistJSON = localStorage.getItem("watchlist"); //getItem returns a string that represents the key value, in this case the "watchlist"
+    var watchlist = JSON.parse(watchlistJSON); //since have a string, need to parse out specific values of the array
+    if (watchlist === null) { //this if statement just creates the watchlist array on the first click of an add button
         watchlist = [];
     }
-    watchlist.push(movie);
-    watchlistJSON = JSON.stringify(watchlist);
-    localStorage.setItem("watchlist", watchlistJSON);
+    watchlist.push(movie); //push method puts the currentMovie.imdbID into the watchlist object if the add button is clicked
+    watchlistJSON = JSON.stringify(watchlist); // since we parsed it earlier, need to make the object a string again so it is valid JSON
+    localStorage.setItem("watchlist", watchlistJSON); //adds the key name, "watchlist", to the key value, watchlistJSON, in localStorage
+
+    console.log("this is watchlistJSON " + watchlistJSON);
 }
--- watchlist.js
+++ watchlist.js
@@ -1,10 +1,10 @@
 $(document).ready(() => {
     var watchlistList = JSON.parse(localStorage.getItem("watchlist"));
-    console.log(watchlistList);
+    console.log("this is the parsed 'watchlist' from local storage" + watchlistList);
 
     function renderMovies(watchlist) {
         var movieHTMLs = watchlist.map((currentMovie) => {
-            console.log(currentMovie);
+            console.log("currentMovie" + currentMovie);
             return `<div class="movie-card" style="width: 200px;">
                 <img class="movie-card-img-top" src="${currentMovie.Poster}" alt="Movie card image cap" height="300px" width="200px">
                 <div class="movie-card-body">
@@ -22,3 +22,5 @@ $(document).ready(() => {
 
     $(".movies-container").html(renderMovies(watchlistList));
 })
+
+//use filter to activate the remove button
