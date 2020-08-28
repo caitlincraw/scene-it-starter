ca4d176c14366870caed82a1aeb6bb7d97124358
Caitlin Crawford <caitlin.crawford16@gmail.com>
2020-08-27 21:44:33 -0400

    Remove console logs

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 $(document).ready( () => {
-    console.log("jQuery is Running")
-
     function renderMovies(movieArray) {
         let movieHTMLs = movieArray.map((currentMovie) => {
-            // console.log(currentMovie);
-            // console.log(currentMovie.Title);
+
             return `<div class="movie-card" style="width: 200px;">
                 <img class="movie-card-img-top" src="${currentMovie.Poster}" alt="Movie card image cap" height="300px" width="200px">
                 <div class="movie-card-body">
@@ -15,7 +12,6 @@ $(document).ready( () => {
             </div>`
 
         })
-        console.log(movieHTMLs);
         return movieHTMLs.join("");
     }
     
@@ -24,14 +20,10 @@ $(document).ready( () => {
     e.preventDefault();
 
     let searchString = $(".search-bar").val();
-    // console.log(searchString);
     let urlEncodedSearchString = encodeURIComponent(searchString); // turns spaces into %20
-    // console.log(urlEncodedSearchString);
     fetch(`http://www.omdbapi.com/?apikey=8534d2a7&s=${urlEncodedSearchString}`)
         .then(response => response.json())
         .then((response) => {
-            console.log(response);
-            console.log(response.Search);
             $(".movies-container").html(renderMovies(response.Search));
         })
    });
@@ -52,9 +44,5 @@ function saveToWatchlist(imdbID) {
             localStorage.setItem("watchlist", watchlistJSON); 
 
         })
-
-    // let movie = movieData.find((currentMovie) => { //find method only returns a single element; use filter method if want to return multiple values
-    //     return currentMovie.imdbID === imdbID; //compares currentMovie.imdb and returns value only if the same as currentMovie.imdbID within the onclick event
-    // });
 
 }
--- watchlist.js
+++ watchlist.js
@@ -1,10 +1,8 @@
 $(document).ready(() => {
     let watchlistList = JSON.parse(localStorage.getItem("watchlist"));
-    console.log("this is the parsed 'watchlist' from local storage" + watchlistList);
 
     function renderMovies(watchlist) {
         let movieHTMLs = watchlist.map((currentMovie) => {
-            console.log("currentMovie" + currentMovie);
             return `<div class="movie-card" style="width: 200px;">
                 <img class="movie-card-img-top" src="${currentMovie.Poster}" alt="Movie card image cap" height="300px" width="200px">
                 <div class="movie-card-body">
@@ -16,11 +14,8 @@ $(document).ready(() => {
     
         })
 
-        console.log(movieHTMLs);
         return movieHTMLs.join("");
     }
 
     $(".movies-container").html(renderMovies(watchlistList));
 })
-
-//use filter to activate the remove button
