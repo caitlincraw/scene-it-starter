e7c7cb11962127abf1b62a1ac59ec00e8c380021
Caitlin Crawford <caitlin.crawford16@gmail.com>
2020-08-27 21:05:42 -0400

    Fix savetoWatchlist

--- index.js
+++ index.js
@@ -39,18 +39,22 @@ $(document).ready( () => {
 })
 
 function saveToWatchlist(imdbID) {
-    let movie = movieData.find((currentMovie) => { //find method only returns a single element; use filter method if want to return multiple values
-        return currentMovie.imdbID === imdbID; //compares currentMovie.imdb and returns value only if the same as currentMovie.imdbID within the onclick event
-    });
+    fetch(`http://www.omdbapi.com/?apikey=8534d2a7&i=${imdbID}`)
+        .then(response => response.json())
+        .then((response) => {
+            let watchlistJSON = localStorage.getItem("watchlist");
+            let watchlist = JSON.parse(watchlistJSON); 
+            if (watchlist === null) { 
+                watchlist = [];
+            }
+            watchlist.push(response); 
+            watchlistJSON = JSON.stringify(watchlist); 
+            localStorage.setItem("watchlist", watchlistJSON); 
 
-    let watchlistJSON = localStorage.getItem("watchlist"); //getItem returns a string that represents the key value, in this case the "watchlist"
-    let watchlist = JSON.parse(watchlistJSON); //since have a string, need to parse out specific values of the array
-    if (watchlist === null) { //this if statement just creates the watchlist array on the first click of an add button
-        watchlist = [];
-    }
-    watchlist.push(movie); //push method puts the currentMovie.imdbID into the watchlist object if the add button is clicked
-    watchlistJSON = JSON.stringify(watchlist); // since we parsed it earlier, need to make the object a string again so it is valid JSON
-    localStorage.setItem("watchlist", watchlistJSON); //adds the key name, "watchlist", to the key value, watchlistJSON, in localStorage
+        })
 
-    console.log("this is watchlistJSON " + watchlistJSON);
+    // let movie = movieData.find((currentMovie) => { //find method only returns a single element; use filter method if want to return multiple values
+    //     return currentMovie.imdbID === imdbID; //compares currentMovie.imdb and returns value only if the same as currentMovie.imdbID within the onclick event
+    // });
+
 }
